aa67afd0094bb76d87c66413fe21a8e5a2fb6eab
nunabek <nagimasultanbekkyzy711@gmail.com>
2024-06-01 18:48:36 +0600

    .

--- mcarkg.py
+++ mcarkg.py
@@ -1,52 +1,65 @@
-import csv
+
+
 import requests
 from bs4 import BeautifulSoup
+import csv
+
+def get_html(url):
+    response = requests.get(url)
+    return response.text
+
+def get_total_pages(html):
+    soup = BeautifulSoup(html, 'lxml')
+    list_page = soup.find('ul', class_ = 'pagination').find_all('li')
+    last_page = list_page[-1]
+    total_pages = last_page.find('a').get('href').split('=')
+    return int(total_pages[-1])
+
 
 def write_to_csv(data):
-    with open('mashina_kg.csv', 'a', newline='', encoding='utf-8') as csv_file:
-        writer = csv.writer(csv_file, delimiter='/')
-        writer.writerow([data['title'], data['price'], data['photo'], data['description']])
+    with open('mashina_kg.csv','a') as f:
+        writer = csv.writer(f)
+        writer.writerow([data['title'],data['price'],data['img'],data['description']])
 
-def get_page_data(html):
-    soup = BeautifulSoup(html, 'lxml')
-    product_list = soup.find('div', class_="listing").find_all('div', class_="list-item list-label")
-    
-    for product in product_list:
-        try:
-            photo = product.find('img').get('data-src', '')
-        except AttributeError:
-            photo = ''
 
+def get_title(html):
+    soup = BeautifulSoup(html,'lxml')
+    block_titles = soup.find_all('div', class_ = 'list-item list-label')
+    # a = []
+    for titles in block_titles:
         try:
-            title = product.find('h2').text.strip()
-        except AttributeError:
+            title = titles.find('div', class_='block title').find('h2',class_ = 'name').text.strip()
+        except:
             title = ''
-
         try:
-            price = product.find('strong', class_="price").text.strip()
-        except AttributeError:
+            price = titles.find('div',class_ ='block price').find('p').find('strong').text
+        except:
             price = ''
-
         try:
-            description = product.find('p').text.strip()
-        except AttributeError:
+            img = titles.find('img').get('data-src')
+        except:
+            img = ''
+        try:
+            description = titles.find('div', class_ ='block info-wrapper item-info-wrapper').find('p',class_ ='year-miles').text.strip()
+        except:
             description = ''
-
-        data = {'title': title, 'price': price, 'photo': photo, 'description': description}
-        print(data)  
+        
+        data = {'title': title, 'price':price, 'img':img, 'description':description}
         write_to_csv(data)
 
 def main():
-    base_url = 'https://www.mashina.kg/search/all/'
-    pages = 5 
+    url_ = 'https://www.mashina.kg/search/all/'
+    pages ='?page='
+    html_ = get_html(url_)
+    number = get_total_pages(html_)
 
-    for page in range(1, pages + 1):
-        url = f"{base_url}?page={page}"
-        response = requests.get(url)
-        if response.status_code == 200:
-            get_page_data(response.text)
-        else:
-            print(f"Error: Unable to fetch page {page}")
+    for i in range(1, number+1):
+        url_pages = url_ + pages + str(i)
+        html = get_html(url_pages)
+        get_title(html)
+   
+# Это было сложно :((
 
-if __name__ == "__main__":
-    main()
+main()
+
+
